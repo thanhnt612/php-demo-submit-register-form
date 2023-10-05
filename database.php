add4a90d0bb27afad9e033da69c46926bb62531c
thanhnt612 <nguyentruongthanh0612@gmail.com>
2023-10-05 17:14:24 +0700

    php demo

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "bookdb";
+$connect = "";
+try {
+    $connect = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+} catch (mysqli_sql_exception) {
+    echo "Could not connect !!! <br>";
+}
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+include("database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+
+<body>
+    <h2>Welcome to MY WEBSITE</h2>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        Username: <br>
+        <input type="text" name="username"><br>
+        Password: <br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="register" value="register">
+    </form>
+</body>
+
+</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    if (empty($username)) {
+        echo "Please enter a username";
+    } elseif (empty($password)) {
+        echo "Please enter a password";
+    } else {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (username, password) VALUES ('$username','$hash')";
+       try {
+        mysqli_query($connect, $sql);
+        echo "You are now register";
+       } catch (mysqli_sql_exception) {
+        echo "That username is taken !!!";
+       }
+    }
+}
+mysqli_close($connect);
+?>
